78e3f475caa43b63ba3dfc28916d779e5975b83e
mpagnis <mrunal.pagnis@oracle.com>
2019-12-30 15:33:34 +0530

    find duplicate lines with the counts from console input

---
+++ basics/FindDuplicateLines.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	counts := make(map[string]int)
+	input := bufio.NewScanner(os.Stdin)
+	for input.Scan() {
+		if input.Text() == "exit" {
+			break
+		}
+		counts[input.Text()]++
+	}
+	for line, count := range counts {
+		if count > 1 {
+			fmt.Printf("%s :\t%d\n",line,count)
+		}
+	}
+}
